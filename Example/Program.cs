776130c4aed117a844dabbd0596700ef297f92f1
Alexander Nyquist <nyquist.alexander@gmail.com>
2014-02-25 08:00:19 +0100

    Downgraded to .NET 4.0. Added Example project

---
+++ Example/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Harvest.ExcludeFilters;
+
+namespace Harvest.Tryouts
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var crawler = new Crawler
+            {
+                ExcludeFilters = new IExcludeFilter[]
+                {
+                    new ExcludeImagesFilter(),
+                    new ExcludeTrackbacks(),
+                    new ExcludeMailTo(),
+                    new ExcludeHostsExcept(new[] { "nyqui.st" }),
+                    new ExcludeJavaScript(), 
+                    new ExcludeAnchors(), 
+                }
+            };
+                
+            crawler.OnCompleted += () =>
+            {
+                Console.WriteLine("[Main] Crawl completed!");
+                Environment.Exit(0);
+            };
+
+            crawler.OnPageDownloaded += page =>
+            {
+                Console.WriteLine("[Main] Downloaded page {0}", page.Url);
+
+                // Write external links
+                foreach (var link in page.Links)
+                {
+                    if (link.TargetUrl.Host != page.Url.Host)
+                    {
+                        Console.WriteLine("Found outbound link from {0} to {1}", page.Url, link.TargetUrl);
+                    }
+                }
+            };
+
+            crawler.Enqueue(new Uri("http://nyqui.st"));
+            crawler.Start();
+
+            Console.WriteLine("[Main] Crawler started.");
+            Console.WriteLine("[Main] Press [enter] to abort.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Harvest.Console")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Harvest.Console")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c2fa9348-551f-4fe4-af43-c6f509e2d515")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
